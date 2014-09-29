47d8b599222b033c8d4d1f1b39f7e681b096d661
LoveIsGrief <just.another.michaelv@gmail.com>
2014-09-29 22:04:15 +0200

    Save db after server-end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("coffee-script/register");
 
 var express = require("express"),
+  fs = require("fs"),
   config = require("./config/config");
 
 var app = express();
@@ -13,5 +14,20 @@ var io = require("socket.io")(http)
 // configure socket.io
 require("./config/socket")(app, io);
 
-http.listen(config.port);
+http.listen(config.port, function () {
+	console.log("Listening on port:" + config.port)
+});
 
+// Save the db before being killed
+function handleShutdown () {
+	console.log("Saving db in face of imminent death");
+
+	fs.writeFileSync(config.db,
+		JSON.stringify(app.get("chatrooms"), null, '\t')
+		)
+	console.log("We may die now")
+	process.exit();
+}
+
+process.on("SIGINT", handleShutdown)
+process.on("SIGTERM", handleShutdown)
